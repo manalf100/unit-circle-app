5a50ffadefab57a4c740ca3745e71f85a4a6f693
manalf100 <ahmedderaz100@gmail.com>
2026-08-18 02:31:03 +0300

    Modify app message for implicit differentiation
    Updated the app message to indicate readiness for lesson plans.

--- differentiate.py
+++ differentiate.py
@@ -1,11 +1,6 @@
 import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
 
-st.set_page_config(page_title="Implicit Differentiation", layout="wide")
+st.set_page_config(page_title="Math Grade 11: Implicit Differentiation", layout="wide")
 
 st.title("Math Grade 11: Implicit Differentiation Interactive App")
-st.write("Explore implicit differentiation concepts interactively.")
-
-x_val = st.slider("Select x value", -5.0, 5.0, 1.0)
-st.write(f"The selected value of x is: {x_val}")
+st.write("App is working successfully and ready for the lesson plans.")
