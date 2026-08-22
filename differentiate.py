88ec4be95aaf3b4ced439af6fc410181cd2354a1
manalf100 <ahmedderaz100@gmail.com>
2026-08-22 16:09:44 +0300

    Delete differentiate.py

--- differentiate.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Math Grade 11: Implicit Differentiation", layout="wide")
-
-st.title("Math Grade 11: Implicit Differentiation Interactive App")
-st.write("App is working successfully and ready for the lesson plans.")
